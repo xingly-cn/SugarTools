0cd45e466c5065bc1e67bcd592abf8c1d92f0111
sugar <sugar_tech@outlook.com>
2022-02-04 15:13:15 +0800

    启动类

---
+++ sugar-auth/src/main/java/com/sugar/auth/AuthApplication.java
@@ -0,0 +1,13 @@
+package com.sugar.auth;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class,args);
+    }
+}
