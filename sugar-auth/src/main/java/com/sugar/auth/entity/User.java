799b4d749f4b0e92986ee7b48dec3d6f5367241d
sugar <sugar_tech@outlook.com>
2022-02-05 19:07:25 +0800

    逻辑删除

--- sugar-auth/src/main/java/com/sugar/auth/entity/User.java
+++ sugar-auth/src/main/java/com/sugar/auth/entity/User.java
@@ -1,15 +1,17 @@
 package com.sugar.auth.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
-import java.util.Date;
 import com.baomidou.mybatisplus.annotation.TableId;
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * <p>
@@ -48,6 +50,7 @@ public class User implements Serializable {
     private Boolean isDisabled;
 
     @ApiModelProperty(value = "逻辑删除 1（true）已删除， 0（false）未删除")
+    @TableLogic
     private Boolean isDeleted;
 
     @ApiModelProperty(value = "创建时间")
